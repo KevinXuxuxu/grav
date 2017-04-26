b747083ac22c8f385a82e34910d30a6ba8793058
callMeName <rickyxuusa@gmail.com>
2017-04-26 14:00:17 -0700

    fix a\b order issue

--- src/gravity.cpp
+++ src/gravity.cpp
@@ -77,8 +77,8 @@ void collide(const Vect &a_c, const Vect &a_v, float a_m,
     vat = ar + Al * e;
     vbt = br + Bl * e;
 }
-
-Vect caldv(const Vect &a_c,const  Vect &a_v,const  float a_m,const Vect &b_c, const Vect &b_v) //impulse of a on b
+/* impulse of a on b */
+Vect caldv(const Vect &b_c,const  Vect &b_v,const Vect &a_c, const Vect &a_v, const  float a_m)
 {
     Vect dx = a_c - b_c;
     Vect dv = a_v - b_v;
@@ -89,7 +89,8 @@ Vect caldv(const Vect &a_c,const  Vect &a_v,const  float a_m,const Vect &b_c, co
                       3.0 / 2.0 * (dx & dv) * dx / (r * r * r * r * r) * dt * dt
                       );
 }
-Vect caldx(const Vect &a_c,const  Vect &a_v,const  float a_m,const Vect &b_c, const Vect &b_v) //impulse of a on b
+/* impulse of a on b */
+Vect caldx(const Vect &b_c,const  Vect &b_v,const Vect &a_c, const Vect &a_v, const  float a_m) 
 {
     Vect dx = a_c - b_c;
     Vect dv = a_v - b_v;
@@ -117,8 +118,8 @@ void iterate2(Vect* cs, Vect* vs, float* ms, float* sizes, Vect *dv, Vect *dx)
         {
             if(j!=i)
             {
-                dv[i] += caldv(cs[i], vs[i], ms[i], cs[j], vs[j]);
-                dx[i] += vs[i] * dt + caldx(cs[i], vs[i], ms[i], cs[j], vs[j]);
+                dv[i] += caldv(cs[i], vs[i], cs[j], vs[j], ms[j]);
+                dx[i] += vs[i] * dt + caldx(cs[i], vs[i], cs[j], vs[j], ms[j]);
             }
         }
     }
